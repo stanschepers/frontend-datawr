886cd1980cf6cb65c896a44eefab86f5a292e5cd
luckasdc <mail@luckasdc.be>
2018-04-18 09:45:04 +0200

    Statistics added Transformations part 1 added

--- client/views/datasets/Statistics.vue
+++ client/views/datasets/Statistics.vue
@@ -1,12 +1,16 @@
 <template>
-    <nav class="level ">
+    <div>
+    <nav class="level has-text-left" v-bind:class="{'is-hidden' : columnsOfType().length === 0}">
         <div class="level-item has-text-left">
-            <div class="select">
-                <label>
-                <select v-on:change="updateStats()" v-model="column1">
-                    <option v-for="heading in columntypes">{{heading.name}}</option>
-                </select>
-                </label>
+            <div class="field">
+                <label class="label">numerical stats</label>
+                <div class="control">
+                    <div class="select">
+                        <select v-on:change="updateNumericalStats()" v-model="column1">
+                            <option v-for="heading in columnsOfType()" v-bind:selected="heading === columnsOfType()[0]">{{heading.name}}</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="level-item has-text-centered">
@@ -34,6 +38,47 @@
             </div>
         </div>
     </nav>
+    <nav class="level" v-bind:class="{'is-hidden' : columnsOfType().length !== 0}">
+        <div class="content has-text-centered">
+            <p><i> There are no numerical columns in this dataset.</i></p>
+        </div>
+    </nav>
+    <nav class="level" v-bind:class="{'is-hidden' : columntypes.length === 0}">
+        <div class="level-item has-text-left">
+            <div class="field">
+                <label class="label">other stats</label>
+                <div class="control">
+                    <div class="select">
+                        <select v-on:change="updateStats()" v-model="column2">
+                            <option v-for="heading in columntypes">{{heading.name}}</option>
+                        </select>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="level-item has-text-centered">
+            <div>
+                <p class="heading">Mode</p>
+                <p class="Title">{{mode}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered">
+            <div>
+                <p class="heading">Empty values</p>
+                <p class="title">{{amount_empty}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered">
+            <div>
+                <p class="heading">Nonempty values</p>
+                <p class="title">{{amount_nonempty}}</p>
+            </div>
+        </div>
+    </nav>
+
+
+    </div>
 </template>
 
 <script>
@@ -46,41 +91,72 @@
         data () {
             return {
                 column1: {name: null, type: null},
+                column1Test: null,
 
                 max: null,
                 min: null,
                 mean: null,
                 median: null,
 
+                column2: {name: null, type: null},
+                mode: null,
+                amount_empty: null,
+                amount_nonempty: null,
+
+
+
 
 
             }
         },
 
+
+
         methods: {
 
-            updateStats() {
-                console.log(this.column1.type)
-                this.$http.get( 'data/stats/' + '?dataset_id=' + this.setid+ '&type=' + 'all' + '&column=' + this.column1).then((response) => {
-                    console.log(response.data);
+            updateNumericalStats() {
+                this.$http.get( 'data/stats/' + '?dataset_id=' + this.setid+ '&type=' + 'all_numerical_stats' + '&column=' + this.column1).then((response) => {
                     this.max = response.data['max']
                     this.min = response.data['min']
                     this.mean = response.data['mean']
                     this.median = response.data['median']
 
-
+                }).catch((error) => {
+                    this.error = true
+                });
+            },
+            updateStats() {
+                this.$http.get( 'data/stats/' + '?dataset_id=' + this.setid+ '&type=' + 'all_stats' + '&column=' + this.column2).then((response) => {
+                    this.mode = response.data['mode']
+                    this.amount_empty = response.data['amount_empty']
+                    this.amount_nonempty = response.data['amount_nonempty']
 
                 }).catch((error) => {
                     this.error = true
                 });
-            }
+            },
+
+            columnsOfType() {
+                let list = []
+                for(const column of this.columntypes) {
+
+                    if(column.type === 'bigint' || column.type === 'double precision'){
+                        list.push(column)
+                    }
+                }
+                return list;
+            },
 
 
 
-        }
+        },
+
+        created() {
+
+        },
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
--- client/views/tables/ColumnTransformations.vue
+++ client/views/tables/ColumnTransformations.vue
@@ -3,81 +3,162 @@
     <div>
         <div class="block">
 
-            <div class="steps" id="steps">
-                <div class="step-item"
-                     v-bind:class="{ 'is-active': activeStep<=1, 'is-completed': activeStep > 1 }">
-                    <div class="step-marker">1</div>
-                    <div class="step-details">
-                        <p class="step-title">Select Column</p>
+            <div class="level is-mobile">
+                <div class="level-left">
+                    <div class="level-item">
+                        <span class="title level-item">Transformations</span>
                     </div>
                 </div>
+            </div>
 
-                <div class="step-item"
-                     v-bind:class="{ 'is-active': activeStep===2, 'is-completed': activeStep > 2  }">
-                    <div class="step-marker">2</div>
-                    <div class="step-details">
-                        <p class="step-title">Select Transformation</p>
-                    </div>
+            <div class="tabs is-centered">
+                <ul>
+                    <li v-bind:class="{'is-active' : selected === 'general'}" v-on:click="selected = 'general'" ><a>General</a></li>
+                    <li v-bind:class="{'is-active' : selected === 'numerical'}" v-on:click="selected = 'numerical'"><a>Numerical</a></li>
+                    <li v-bind:class="{'is-active' : selected === 'datetime'}" v-on:click="selected = 'datetime'"><a>Date/Time </a></li>
+                    <li v-bind:class="{'is-active' : selected === 'categorical'}" v-on:click="selected = 'categorical'"><a>Categorical</a></li>
+                    <li v-bind:class="{'is-active' : selected === 'text'}" v-on:click="selected = 'text'"><a>Text</a></li>
+                </ul>
+            </div>
+
+                <div class="block" title="Table changes" v-bind:class="{'is-hidden' : selected !== 'general'}">
+                   <general :columntypes="columntypes" :setid="setid">
+
+                   </general>
                 </div>
 
-                <div class="step-item"
-                     v-bind:class="{ 'is-active': activeStep>=3 }">
-                    <div class="step-marker">3</div>
-                    <div class="step-details">
-                        <p class="step-title">Results</p>
-                    </div>
-                </div>
+                <div class="block" title="Numerical transformations" v-bind:class="{'is-hidden' : selected !== 'numerical'}">
+                    <collapse accordion>
+                        <collapse-item title="Normalise">
+                            <div class="block">
+                                Normalise numerical columns between 0 and 1.
 
-            <div class="steps-content">
+                                <div class="control is-horizontal">
+                                    <div class="control-label">
+                                        <label class="label">Column</label>
+                                    </div>
+                                    <div class="control">
+                                        <div class="select is-fullwidth">
+                                            <select>
+                                                <option>registration_id</option>
+                                                <option>client_id</option>
+                                                <option>checkin_time</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
 
-                <div class="step-content has-text-centered"
-                     v-bind:class="{ 'is-active': activeStep===1, 'is-hidden': activeStep !== 1 }">
+                                <div class="control is-horizontal">
+                                    <div class="control-label">
+                                        <label class="label">Mean</label>
+                                    </div>
+                                    <div class="control is-grouped">
+                                        <p class="control is-expanded">
+                                            <input class="input" type="number" placeholder="mean">
+                                        </p>
+                                    </div>
+                                </div>
 
-                    <div class="field">
-                        <div class="control is-grouped has-text-centered">
-                            <div class="control-label">
-                                <label class="label">Select column to change</label>
-                                <div class="select">
-                                    <select>
-                                        <option v-for="heading in columntypes">{{heading.name}}</option>
+                                <div class="control is-horizontal">
+                                    <div class="control-label">
+                                        <label class="label">Standard deviation</label>
+                                    </div>
+                                    <div class="control is-grouped">
+                                        <p class="control is-expanded">
+                                        <p></p>
+                                        <input class="input" type="number" placeholder="standard deviation">
+                                        </p>
+                                    </div>
+                                </div>
 
-                                    </select>
+
+                                <div class="control is-grouped">
+                                    <div class="control-label">
+                                        <label class="label"></label>
+                                    </div>
+                                    <div class="control">
+                                        <button class="button is-primary">Normalise</button>
+                                    </div>
                                 </div>
                             </div>
 
-                        </div>
+
+
+
+                        </collapse-item>
+                        <collapse-item title="Discretise">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. @bulmaio. #css #responsive
+                        </collapse-item>
+                        <collapse-item title="Remove outliers">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. @bulmaio. #css #responsive
+                        </collapse-item>
+                    </collapse>
+                </div>
+
+                <div class="block" title="Date | time parsing" v-bind:class="{'is-hidden' : selected !== 'datetime'}">
+                    <collapse accordion>
+                        <collapse-item title="Parse date/time values">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. @bulmaio. #css #responsive
+                        </collapse-item>
+                        <collapse-item title="....">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. @bulmaio. #css #responsive
+                        </collapse-item>
+                    </collapse>
+                </div>
+
+                <div class="block" title="One-hot-encoding" v-bind:class="{'is-hidden' : selected !== 'categorical'}">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. @bulmaio. #css #responsive
+                </div>
+
+                <div class="block" title="Find-and-replace" v-bind:class="{'is-hidden' : selected !== 'text'}">
+                    <div class="field">
+                        <p class="control">
+                            <input class="input" type="text" placeholder="Find">
+                        </p>
                     </div>
+
+                    <div class="field">
+                        <p class="control">
+                            <input class="input" type="text" placeholder="Replace by">
+                        </p>
+                    </div>
+
+                    <div class="field">
+                        <p class="control">
+
+                        <div class="field is-grouped">
+                            <div class="b-checkbox is-default is-inline">
+                                <input id="matchcase" class="styled" checked type="checkbox">
+                                <label for="matchcase">
+                                    Match case
+                                </label>
+                            </div>
+                            <div class="b-checkbox is-default is-inline">
+                                <input id="word" class="styled" checked type="checkbox">
+                                <label for="word">
+                                    Word
+                                </label>
+                            </div>
+                            <div class="b-checkbox is-default is-inline">
+                                <input id="regex" class="styled" checked type="checkbox">
+                                <label for="regex">
+                                    Regex
+                                </label>
+                            </div>
+                        </div>
+                        <div class="field is-grouped">
+                            <div class="block">
+                                <button class="button is-primary">Find</button>
+                                <button class="button is-primary is-outlined">Find & Replace</button>
+                                <button class="button is-primary is-outlined">Replace All</button>
+                            </div>
+                        </div>
+                        </p>
+                    </div>
+
                 </div>
 
-                <div class="step-content has-text-centered"
-                     v-bind:class="{ 'is-active': activeStep===2, 'is-hidden': activeStep !== 2  }">
-
-                    <h1 class="title is-4">Second Step !</h1>
-                </div>
-
-                <div class="step-content has-text-centered"
-                     v-bind:class="{ 'is-active': activeStep===3, 'is-hidden': activeStep !== 3  }">
-
-                    <h1 class="title is-4">Final Step !</h1>
-                </div>
-
-            </div>
-
-            <div class="steps-actions">
-
-                <div class="steps-action" v-on:click="incrementStep(-1)">
-                    <a class="button" v-bind:class="{'is-disabled': activeStep===1}" >Previous</a>
-                </div>
-                <div class="steps-action" v-on:click="incrementStep(1)">
-                    <a class="button" v-bind:class="{'is-disabled': activeStep===3}" >Next</a>
-                </div>
-
-            </div>
-
-
-            </div>
         </div>
-
     </div>
 
 
@@ -85,17 +166,21 @@
 
 <script>
 
+    import { Collapse, Item as CollapseItem } from 'vue-bulma-collapse'
+    import general  from '../transformations/General'
 
     export default {
-        components: {},
+        components: {CollapseItem, Collapse, general},
         props: {
             columntypes: Array,
+            setid: Number,
         },
         name: "column-transformations",
         data() {
             return {
                 activeStep: 1,
                 columnTypes: [],
+                selected: 'general',
             }
         },
         methods: {
